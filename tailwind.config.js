cd450c461fd1a4f40bf106baf5c89cbb68fccd01
AHMEDmohamedyassin <ahmedmohamed982025@outlook.com>
2024-08-27 20:24:02 +0300

    v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: false, // Do not center the container
+      padding: '0', // Remove default padding
+      screens: {}, // Remove default max-width values for breakpoints
+    },
     extend: {
       colors:{
         mainColor : "#374eef" ,
@@ -10,6 +15,9 @@ module.exports = {
         bgColor : "#ffffff"
       }
     },
+  },
+  corePlugins: {
+    container: false, // Disable the container plugin if you want to completely remove it
   },
   plugins: [],
 }
